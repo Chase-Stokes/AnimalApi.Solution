25e29384d0683b5139b144b6901bd81a5e87ec67
Chase Stokes <chasehunterstokes@gmail.com>
2022-04-01 09:01:05 -0700

    add project structure

---
+++ AnimalApi/Models/Animal.cs
@@ -0,0 +1,12 @@
+namespace AnimalApi.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Species { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public string Sex { get; set; }
+    public int Age { get; set; }
+  }
+}
